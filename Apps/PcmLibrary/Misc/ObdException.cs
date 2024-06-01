a02a0b057d8983cf8cfe89f7aeaaea27be3f0c5c
NSFW <nsfw@outlook.com>
2024-05-31 17:40:48 -0700

    Added ObdException class

---
+++ Apps/PcmLibrary/Misc/ObdException.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PcmHacking
+{
+    public enum ObdExceptionReason
+    {
+        /// <summary>
+        /// Unspecified error type - try to avoid using this.
+        /// </summary>
+        Error = 0,
+
+        /// <summary>
+        /// Response was shorter than expected.
+        /// </summary>
+        Truncated = 1,
+
+        /// <summary>
+        /// Response contained data that differs from what was expected.
+        /// </summary>
+        UnexpectedResponse = 2,
+
+        /// <summary>
+        /// No response was received before the timeout expired.
+        /// </summary>
+        Timeout = 3,
+
+        /// <summary>
+        /// The operation was cancelled by the user.
+        /// </summary>
+        Cancelled = 4,
+
+        /// <summary>
+        /// The request was refused.
+        /// </summary>
+        Refused = 5,
+    }
+
+    internal class ObdException : Exception
+    {
+        public ObdExceptionReason Reason { get; private set; }
+
+        public ObdException(string message, ObdExceptionReason reason) : base(message)
+        {
+            this.Reason = reason;
+        }
+    }
+}
